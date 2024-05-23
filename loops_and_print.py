8f560ad5334ceb5d525d1888370fe4e410e75802
Bautista-Maurizio <83047373+Bautista-Maurizio@users.noreply.github.com>
2024-05-23 14:29:21 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,31 @@
 def enumerate_list(list):
-    return list
+    New_list1 = []
+    a = list.count("")
+    while a >= 1:
+        list.remove("")
+        a= list.count("")
+
+    if a == 0:
+        for index in range(len(list)):
+            New_list1.append(f"{index}. {list[index]}")
+        return New_list1
+
+
+
+
 
 
 def enumerate_backwards(list):
-    return list
+    New_list1 = []
+    New_list2 = []
+    a = list.count("")
+    while a >= 1:
+        list.remove("")
+        a = list.count("")
+
+    if a == 0:
+        for b in list:
+            New_list1.append(b[::-1])
+        for item, b in enumerate(New_list1[0:]):
+            New_list2.append(f"{item}. {b}")
+        return New_list2
